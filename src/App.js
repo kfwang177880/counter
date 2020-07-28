ec2cb1fb3f6d9197ed2182a437b202faf1e3d14f
kfwang177880 <kfwang177880@yahoo.com>
2020-07-28 10:53:01 -0400

    first version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+class Counter extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 0,
+      doubleCount: false,
+    };
+  }
+  //this is where our methods will be defined
+  increment = () => {
+     // this is where your code goes
+    // could have done with one condition for both increment and decrement !!!
+    // decrement would have been ( this.state.count > 0)
+    // if (this.state.count < 20){
+    // this.setState({
+    //  count: this.state.count +1
+    // })
+    // }
+    if (this.state.doubleCount ===  false && this.state.count < 20) {
+    this.setState({
+      count: this.state.count + 1,
+        }); 
+    }else if (this.state.doubleCount === true && this.state.count <19){
+      this.setState({
+        count: this.state.count +2
+      })
+    } 
+  };
+    // this is where your code goes
+  
+  decrement = () => {
+  
+    if(!this.state.doubleCount && this.state.count > 0){
+  //if(this.state.doubleCount === false && this.state.count > 0){
+      this.setState({
+        count: this.state.count -1,
+      })
+//    }else if (this.state.doubleCount === true && this.state.count > 1){
+    } else if (this.state.doubleCount  && this.state.count > 1){ 
+      this.setState({
+        count: this.state.count -2
+      })
+    }
+  }
+    
+  clear = () => {
+    this.setState({
+      count: this.state.count = 0.
+    });
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  toggle = () => {
+    this.setState({
+      doubleCount: !this.state.doubleCount,
+    })    
+  }
+
+  render () {
+    return(
+      <div className = "container"> 
+        <div className = "navbar"> Counter.js</div> 
+        <div className ="counter">
+          <h1>{this.state.count}</h1>
+          <button type = "button" onClick={this.increment}>Increment</button>
+          <button type = "button" onClick={this.decrement}>Decrement</button>
+          <button type = "button" onClick={this.clear}>clear</button>
+          <button type = "button" onClick={this.toggle}>
+            {this.state.doubleCount ? 'Double Count' : 'Single Count'}</button>
+          
+        </div>
+      
+      </div>
+    );
+  }
 }
-
-export default App;
+export default Counter;
